4757a351837a2241395bd9cdf7222265362a56c2
Alexander Lukyanchikov <al@leads.su>
2014-12-07 20:56:50 +0400

    Added  ionrange widget

---
+++ widgets/IonRangeSlider.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * @link http://yii2metronic.icron.org/
+ * @copyright Copyright (c) 2014 icron.org
+ * @license http://yii2metronic.icron.org/license.html
+ */
+
+namespace icron\metronic\widgets;
+
+use Yii;
+use yii\helpers\Html;
+use yii\helpers\Json;
+use yii\web\View;
+
+/**
+ *  IonRangeSlider renders ionRangeSlider widget.
+ *
+ *  For example, if [[model]] and [[attribute]] are not set:
+ * ```php
+ * echo IonRangeSlider::widget([
+ *     'name' => 'ionRangeSlider',
+ *     'clientOptions' => [
+ *         'min' => 0,
+ *         'max' => 5000,
+ *         'from' => 1000, // default value
+ *         'to' => 4000, // default value
+ *         'type' => 'double',
+ *         'step' => 1,
+ *         'prefix' => "$",
+ *         'prettify' => false,
+ *         'hasGrid' => true
+ *     ],
+ * ]);
+ * ```
+ * @see https://github.com/IonDen/ion.rangeSlider
+ */
+class IonRangeSlider extends InputWidget
+{
+    /**
+     * @var string separator values
+     */
+    public $separator = ';';
+
+    /**
+     * Executes the widget.
+     */
+    public function run()
+    {
+        if ($this->hasModel()) {
+            $values = explode($this->separator, $this->model->{$this->attribute});
+            if (count($values) == 2) {
+                $this->clientOptions['from'] = (int)$values[0];
+                $this->clientOptions['to'] = (int)$values[1];
+            }
+            echo Html::activeTextInput($this->model, $this->attribute, $this->options);
+        } else {
+            $values = explode($this->separator, $this->value);
+            if (count($values) == 2) {
+                $this->clientOptions['from'] = (int)$values[0];
+                $this->clientOptions['to'] = (int)$values[1];
+            }
+            echo Html::textInput($this->name, $this->value, $this->options);
+        }
+        IonRangeSliderAsset::register($this->view);
+        $this->registerPlugin('ionRangeSlider');
+    }
+
+}
---
+++ widgets/IonRangeSliderAsset.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @link http://yii2metronic.icron.org/
+ * @copyright Copyright (c) 2014 icron.org
+ * @license http://yii2metronic.icron.org/license.html
+ */
+
+namespace  icron\metronic\widgets;
+
+use yii\web\AssetBundle;
+
+/**
+ * IonRangeSliderAsset for slider widget.
+ */
+class IonRangeSliderAsset extends AssetBundle
+{
+    public $sourcePath = '@icron/metronic/assets';
+    public $js = [
+        'plugins/ion.rangeslider/js/ion-rangeSlider/ion.rangeSlider.min.js',
+    ];
+
+    public  $css = [
+        'plugins/ion.rangeslider/css/ion.rangeSlider.css',
+        'plugins/ion.rangeslider/css/ion.rangeSlider.Metronic.css',
+    ];
+
+    public $depends = [
+        'icron\metronic\CoreAsset',
+    ];
+}
